ae1841b2ba156959577c3511fa57112742dddba8
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-11-02 11:47:48 +0530

    Implement odd/even check using ternary operator
    Refactored odd/even check to use ternary operator.

---
+++ OddEven.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+//        if(num%2==0){
+//            System.out.println("Even");
+//        }
+//        else{
+//            System.out.println("Odd");
+//        }
+        
+        
+        //using ternary operator
+        String ans;
+        ans=(num % 2 == 0)?"even":"odd";
+        System.out.println(ans);
+    }
+}
