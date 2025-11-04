21bbc7f17314dbeeeec0afa802e3e0ad4f938019
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-11-04 22:22:11 +0530

    Implement while loop to calculate sum of numbers

---
+++ WhileLoop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int num=1;
+        int sum=0;
+
+        while(num<=n){
+//            System.out.println(num);
+            sum = sum + num;
+            num++; // increment operator ++ or same as num=num+1
+        }
+        System.out.println(sum);
+    }
+}
