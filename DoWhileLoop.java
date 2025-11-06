f26a0ea5039070cdd68ea0d10c8879c457b91654
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-11-06 21:48:08 +0530

    Add DoWhileLoop class with main method
    Implement a do-while loop that prints numbers.

---
+++ DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int num=20;
+        do{
+            System.out.println(num);  //this execute atleast once whatever will be the condition
+            num++;
+        }while(num<=10);
+    }
+}
